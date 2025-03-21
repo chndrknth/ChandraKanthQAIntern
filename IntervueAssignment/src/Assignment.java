720f1a6eccdc1874cc4d76d5caca7989d3f37d02
chndrknth <149062418+chndrknth@users.noreply.github.com>
2025-03-21 19:47:28 +0530

    Updated Assignment

--- IntervueAssignment/src/Assignment.java
+++ IntervueAssignment/src/Assignment.java
@@ -1,43 +1,40 @@
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler;
 
-public class Assignment {
-	public static void main(String[] args) throws IOException, InterruptedException 
-	{
-		 
-			System.setProperty("webdriver.chrome.driver","./softwares/chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
-			driver.get("https://www.intervue.io/");
-			driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			driver.findElement(By.xpath("(//span[text()='Login'])[1]")).click();
-			
-			Set<String> wins = driver.getWindowHandles();
-			for(String win:wins){
-				driver.switchTo().window(win);
-			}
-			
-			driver.findElement(By.xpath("(//div[text()='Login'])[1]")).click();
-			driver.findElement(By.id("login_email")).sendKeys("chandrakanthsrinivasa@gmail.com");
-			driver.findElement(By.id("login_password")).sendKeys("password");
-			driver.findElement(By.xpath("//button[@type='submit']")).click();
-			Thread.sleep(2000);  
-			
-		    ////TakesScreenshot ts=(TakesScreenshot)driver;
-		    File temp = ts.getScreenshotAs(OutputType.FILE);
-		    File perm= new File("./Screenshot/login.png");
-		    FileHandler.copy(temp, perm);
-			driver.close();
-		 }
+public class UpdatedIntervueAssignment {
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.intervue.io");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("(//span[text()='Login'])[1]")).click();
+		
+		Set<String> allwindows = driver.getWindowHandles();
+		ArrayList<String> tabs = new ArrayList<String>(allwindows);
+		driver.switchTo().window(tabs.get(1));
+	//driver.manage().timeouts().implicitlyWait(9,TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//a[@href='/login']")).click();
+		driver.findElement(By.name("email")).sendKeys("neha@intervue.io");
+		driver.findElement(By.name("password")).sendKeys("Neha@intervue@123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		Thread.sleep(9000);
+		driver.findElement(By.xpath("//span[text()='Search by candidate name, profile etc.']")).click();
+
+		driver.findElement(By.xpath("//input[@placeholder='Type what you want to search for']")).sendKeys("hello");
+		driver.findElement(By.xpath("(//span[text()='hello'])[1]")).click();
+		Thread.sleep(3000);
+	    driver.findElement(By.xpath("//div[text()='Neha']")).click();
+	    Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Logout']")).click();
+		driver.close();
+
+		
 	}
 
-
+}
